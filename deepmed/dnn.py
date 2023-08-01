2f2a624f631030dc0a6db545c8f469ee1df733f8
Gloriagao0624 <72056756+Gloriagao0624@users.noreply.github.com>
2023-08-01 17:11:14 -0500

    Update dnn.py

--- deepmed/dnn.py
+++ deepmed/dnn.py
@@ -2,36 +2,39 @@ import numpy as np
 import random
 import pandas as pd
 import math
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras import initializers
-from tensorflow.keras.layers import BatchNormalization
+
 import tensorflow as tf
-from tensorflow.keras.layers import Input
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Concatenate
-from tensorflow.keras import regularizers
-from tensorflow.keras import Model
- 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input,Dense,BatchNormalization
+from tensorflow.keras.initializers import GlorotUniform
+from tensorflow.keras.regularizers import l1
 
 
-def make_dnn(train_ybin,input_dim,l1,layers,units):
-    inputs = Input(shape=(input_dim,), name='input')
-    phi = Dense(units=units, activation='relu', kernel_initializer=initializers.glorot_uniform(seed=1),name='phi_1')(inputs)
-    for i in range(layers):
-        phi = Dense(units=units, activation='relu', kernel_initializer=initializers.glorot_uniform(seed=1),name='phi_'+str(i+2))(phi)
-        phi = BatchNormalization()(phi)
-    
-    
+
+def make_dnn(train_ybin,input_dim,lambda_value,layers,units):
+    model = Sequential()
+    i=0
+    while i<layers:
+        if i==0:
+            n_input=input_dim
+            lambda_l1=l1(lambda_value)
+            units_i = units
+        else:
+            n_input = units
+            lambda_l1=None
+            units_i = units
+        model.add(Dense(units = units_i,activation = 'relu', input_shape = (n_input,),
+                        kernel_regularizer = lambda_l1,
+                        kernel_initializer = GlorotUniform(seed=1)))
+        model.add(BatchNormalization())
+        i+=1
     if train_ybin==1:
-        phi = Dense(units=1, activation='sigmoid', kernel_initializer=initializers.glorot_uniform(seed=1))(phi)
-        model = Model(inputs=inputs, outputs=phi)
-        #model.compile(optimizer='adam',loss = 'binary_crossentropy',metrics = ['binary_crossentropy'])
+        model.add(Dense(units = 1,activation = 'sigmoid', kernel_initializer = GlorotUniform(seed=1)))
+        model.compile(optimizer=tf.keras.optimizers.Adam(),loss = 'binary_crossentropy',metrics = ['binary_crossentropy'])
     else:
-        phi = Dense(units=1,kernel_initializer=initializers.glorot_uniform(seed=1))(phi)
-        model = Model(inputs=inputs, outputs=phi)
-        #model.compile(optimizer='adam',loss = "mean_squared_error",metrics = ['mean_squared_error'])
+        model.add(Dense(units = 1, kernel_initializer = GlorotUniform(seed=1)))
+        model.compile(optimizer=tf.keras.optimizers.Adam(),loss = 'mean_squared_error',metrics = ['mean_squared_error'])
+                        
         
     return model
 
@@ -44,16 +47,18 @@ def dnn(y, x, ytest, xtest, hyper):
     batch_size=int(hyper[4])
     input_dim = x.shape[1]
     NN = make_dnn(train_ybin,input_dim,l1,layers,units)
-    if train_ybin==1:
-        NN.compile(optimizer='adam',loss = 'binary_crossentropy',metrics = ['binary_crossentropy'])
-    else:
-        NN.compile(optimizer='adam',loss = "mean_squared_error",metrics = ['mean_squared_error'])
-    NNfit = NN.fit(x, y, epochs =epochs, batch_size=batch_size, verbose = True,validation_data=(xtest, ytest))   
+    # if train_ybin==1:
+    #     NN.compile(optimizer='adam',loss = 'binary_crossentropy',metrics = ['binary_crossentropy'])
+    # else:
+    #     NN.compile(optimizer='adam',loss = "mean_squared_error",metrics = ['mean_squared_error'])
+    NNfit = NN.fit(x, y, epochs =epochs, batch_size=batch_size, verbose = False,validation_data=(xtest, ytest))   
         
     ypred = NN(xtest).numpy().flatten()
     val_loss_all = NNfit.history["val_loss"]
     loss = min(val_loss_all)
     epoch_opt = np.argmin(val_loss_all)
+    if epoch_opt==0:
+        epoch_opt=1
     return loss,ypred,epoch_opt   
     
     
